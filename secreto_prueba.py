b6655d19d12f223f4d9efdb15a224be0b3e71e5d
JuanParraIV <jmparra1993@gmail.com>
2025-05-03 10:27:15 -0500

    Test: agregando secreto para probar gitleaks

--- secreto_prueba.py
+++ secreto_prueba.py
@@ -1,2 +1,4 @@
 # Este archivo contiene un secreto de prueba para verificar la detección de GitLeaks
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+AWS_SECRET_ACCESS_KEY = "ASd123asD123"
