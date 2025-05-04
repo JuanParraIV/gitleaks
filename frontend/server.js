3e2874403c7363f72935614794d39b8daee35cb4
JuanParraIV <jmparra1993@gmail.com>
2025-05-04 13:48:56 -0500

    probando mensaje invalido

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
